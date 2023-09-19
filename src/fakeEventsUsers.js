0ac4bc48a44120ef9a88884069d2ee6050e98f01
Gustav <94684365+Pramell94@users.noreply.github.com>
2023-09-20 00:28:22 +0200

    Added random file with fake users and events

---
+++ src/fakeEventsUsers.js
@@ -0,0 +1,56 @@
+(function () {
+    const users = [
+        {
+            "name": "John Doe",
+            "userid": "1",
+            "country": "Sweden",
+            "role": "admin"
+        },
+        {
+            "name": "Jane Doe",
+            "userid": "2",
+            "country": "USA",
+            "role": "user"
+        },
+        {
+            "name": "Random Hacker",
+            "userid": "3",
+            "country": "Russia",
+            "role": "admin"
+        },
+        {
+            "name": "Finn Balor",
+            "userid": "4",
+            "country": "Ireland",
+            "role": "user"
+        }
+    ]
+});
+
+(function () {
+const events = [
+    {
+        "name": "We blasting",
+        "eventId": "0001",
+        "members": "Random Hacker",
+        "time": "12.00 - 14.00",
+        "date": "18/10/23",
+        "place": "Towers 12"
+    },
+    {
+        "name": "Friday deadline",
+        "eventId": "0002",
+        "members": "Random Hacker",
+        "time": "23.58 - 23.59",
+        "date": "15/10/23",
+        "place": "Towers 12"
+    },
+    {
+        "name": "Monday best day",
+        "eventId": "0003",
+        "members": "Random Hacker",
+        "time": "18.00 - 19.00",
+        "date": "12/10/23",
+        "place": "Towers 12"
+    }
+] });
