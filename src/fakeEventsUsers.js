2fa199f873d8f70c343d258bfe491ae1fc7b670f
Gustav <94684365+Pramell94@users.noreply.github.com>
2023-09-20 00:31:44 +0200

    added const

--- src/fakeEventsUsers.js
+++ src/fakeEventsUsers.js
@@ -25,32 +25,38 @@
             "role": "user"
         }
     ]
+    const events = [
+        {
+            "name": "We blasting",
+            "eventId": "0001",
+            "members": "Random Hacker",
+            "time": "12.00 - 14.00",
+            "date": "18/10/23",
+            "place": "Towers 12"
+        },
+        {
+            "name": "Friday deadline",
+            "eventId": "0002",
+            "members": "Random Hacker",
+            "time": "23.58 - 23.59",
+            "date": "15/10/23",
+            "place": "Towers 12"
+        },
+        {
+            "name": "Monday best day",
+            "eventId": "0003",
+            "members": "Random Hacker",
+            "time": "18.00 - 19.00",
+            "date": "12/10/23",
+            "place": "Towers 12"
+        }
+    ]
 });
 
-(function () {
-const events = [
-    {
-        "name": "We blasting",
-        "eventId": "0001",
-        "members": "Random Hacker",
-        "time": "12.00 - 14.00",
-        "date": "18/10/23",
-        "place": "Towers 12"
-    },
-    {
-        "name": "Friday deadline",
-        "eventId": "0002",
-        "members": "Random Hacker",
-        "time": "23.58 - 23.59",
-        "date": "15/10/23",
-        "place": "Towers 12"
-    },
-    {
-        "name": "Monday best day",
-        "eventId": "0003",
-        "members": "Random Hacker",
-        "time": "18.00 - 19.00",
-        "date": "12/10/23",
-        "place": "Towers 12"
-    }
-] });
+const userListModel = function () {
+    return users;
+  };
+
+const eventListModel = function () {
+    return events;
+};
