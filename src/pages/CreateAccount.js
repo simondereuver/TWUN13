3e472216fc8d370e497d544a1151f65df7df9e56
Simon de Reuver <dereuversimon@gmail.com>
2023-09-24 20:23:15 +0200

    Finished reworking the create account page

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-
+import Button from '@mui/material/Button';
 
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
@@ -10,37 +10,63 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
+import "./CreateAccount.css";
 
 function CreateAccountForm () {
 
-    
+    const [errorPassword, setErrorPassword] = useState(false);
     const [password1, setPassword1] = useState('');
     const [password2, setPassword2] = useState('');
-    const [error, setError] = useState(false);
-    /*
-    DO NOT REMOVE THIS COMMENT, WILL RESULT IN RE-RENDER BUG
-    if (password1 !== password2)
-    {
-      setError(true);
-    }
-    else
-    {
-      setError(false);
-    }
-    */
-    /*
-    export default function InputAdornments() {
-    const [showPassword, setShowPassword] = React.useState(false);
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-    };
-    */
-    const [showPassword, setShowPassword] = React.useState(false);
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const [errorEmail, setErrorEmail] = useState(false);
+    const [userEmail, setEmail] = useState('');
+    //Move to model and server logic once it needs to be implemented
+    const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const [errorUserName, setErrorUserName] = useState(false);
+    const [userName, setUserName] = useState('');
+
     
+    const handleSignUpClick = () => {
+
+        //Put a fetch request here to the server-side once it has been implemented
+        //Maybe return an array with true or false and index into the if-cases to set the error boxes instead.
+
+        //We actually want to send this to the model via controller and handle the sign up logic there
+        //move this to server side, just send the information over and do something similar like this
+        if (password1 !== password2) {
+            setErrorPassword(true);
+            alert(`Password1 is: ${password1} and Password2 is: ${password2}, please make them the same`);
+        } else {
+            setErrorPassword(false);
+        }
+    
+        if (!emailValidation.test(userEmail)) {
+            setErrorEmail(true);
+        } else {
+            setErrorEmail(false);
+        }
+
+        //add check for username in users in server/model for now just set to false
+        if(userName == ""){
+            setErrorUserName(true);
+        } else {
+            setErrorUserName(false);
+        }
+       
+    };
+    
+    
+
+    const [showPassword1, setShowPassword1] = React.useState(false);
+    const [showPassword2, setShowPassword2] = React.useState(false);
+
+    const handleClickShowPassword1 = () => setShowPassword1((show) => !show);
+    const handleClickShowPassword2 = () => setShowPassword2((show) => !show);
+
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
     };
-    
 
     return (
         <Box
@@ -54,94 +80,90 @@ function CreateAccountForm () {
             <div className="create-account">
                 <div>
                     <p>Please fill in information needed to create your account!</p>
+                    <p id="required"> All fields with * needs to be filled in!</p>
+                    
                     <TextField
                         required
                         id="user-name"
-                        label="Required"
-                        defaultValue="Enter username"
+                        label="Username"
+                        placeholder="Enter username"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        error={errorEmail}
+                        helperText={errorEmail ? "Invalid username." : ""}
                     />
-                    {error && (
-                        <TextField
-                            error
-                            id="outlined-error-helper-text"
-                            label="Error"
-                            defaultValue="Hello World"
-                            helperText="Passwords don't match."
-                        />
-                    )}
                 </div>
                 <div>
                     <TextField
                         required
                         id="email-adress"
-                        label="Required"
-                        defaultValue="Enter email"
+                        label="Email"
+                        placeholder="Enter email"
+                        value={userEmail}
+                        onChange={(e) => setEmail(e.target.value)}
+                        error={errorEmail} // set error state
+                        helperText={errorEmail ? "Invalid email address." : ""} 
                     />
-                    {error && (
-                        <TextField
-                            error
-                            id="outlined-error-helper-text"
-                            label="Error"
-                            defaultValue="Hello World"
-                            helperText="Passwords don't match."
+                </div>
+                <div>
+                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-password">Password *</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={showPassword1 ? 'text' : 'password'}
+                            endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword1}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                                >
+                                {showPassword1 ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                            }
+                            label="Password"
+                            placeholder="Enter password"
+                            value={password1}
+                            onChange={(e) => setPassword1(e.target.value)}
+                            error={errorPassword} // set error state
+                            helperText={errorPassword ? "Passwords don't match." : ""}
                         />
-                    )}
+                    </FormControl>
                 </div>
                 <div>
-                    <TextField
-                        required
-                        id="password-1"
-                        label="Required"
-                        defaultValue="Enter password"
-                    />
-                    {error && (
-                    <TextField
-                        error
-                        id="outlined-error-helper-text"
-                        label="Error"
-                        defaultValue="Hello World"
-                        helperText="Passwords don't match."
-                    />
-                    )}
+                    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                        <InputLabel htmlFor="outlined-adornment-password">Password *</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={showPassword2 ? 'text' : 'password'}
+                            endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword2}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                                >
+                                {showPassword2 ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                            }
+                            required
+                            label="Password"
+                            placeholder="Confirm password"
+                            value={password2}
+                            onChange={(e) => setPassword2(e.target.value)}
+                            error={errorPassword} // set error state
+                            helperText={errorPassword ? "Passwords don't match." : ""}
+                        />
+                    </FormControl>
                 </div>
                 <div>
-                    <TextField
-                        required
-                        id="password-2"
-                        label="Required"
-                        defaultValue="Confirm password"
-                    />
-                    {error && (
-                    <TextField
-                        error
-                        id="outlined-error-helper-text"
-                        label="Error"
-                        defaultValue="Hello World"
-                        helperText="Passwords don't match."
-                    />
-                    )}
-                </div>
-                <div>
-                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-password"
-                        type={showPassword ? 'text' : 'password'}
-                        endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            edge="end"
-                            >
-                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                        }
-                        label="Password"
-                />
-                </FormControl>
+                    <Button onClick={handleSignUpClick} variant="outlined">
+                        Sign up!
+                    </Button>
                 </div>
             </div>
         </Box>
