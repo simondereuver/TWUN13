cb0a9ed7e6947dcb9d27039bcfb944b3ff27173b
Gustav <94684365+Pramell94@users.noreply.github.com>
2023-09-25 22:36:00 +0200

    Added so sidepanel slides in

--- src/components/Sidepanel.js
+++ src/components/Sidepanel.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "./Sidepanel.css";
-import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { List, ListItem, ListItemIcon, ListItemText, } from "@mui/material";
 import { useLocation, useNavigate } from 'react-router-dom'
 
-function SidePanel( { SideData } ) {
+function SidePanel( { SideData, open} ) {
     const navigate = useNavigate()
     const location = useLocation()
 
     return (
-        <div className="SidePanel">
-            <List className="SidePanelList">
-                {SideData.map(item => (
-                    <ListItem className="bar"
-                        key={item.text}
-                        onClick={() => navigate(item.path)}
-                        id={location.pathname === item.path ? "active" : null}
-                    >
-                        <ListItemIcon>{item.icon}</ListItemIcon>
-                        <ListItemText primary={item.text}/>
-                    </ListItem>
-                ))}
-            </List>
-        </div>
+            <div className={`SidePanel ${open ? "open" : ""}`}>
+                <List className="SidePanelList">
+                    {SideData.map(item => (
+                        <ListItem className="bar"
+                            key={item.text}
+                            onClick={() => navigate(item.path)}
+                            id={location.pathname === item.path ? "active" : null}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text}/>
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
     );
 }
 
