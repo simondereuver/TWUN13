56bede742cf313980aa1788f2a84cea849838671
Simon de Reuver <dereuversimon@gmail.com>
2023-09-25 23:11:16 +0200

    finished working on the logincomponent for now, visually finished

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from "react";
 import { TextField } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
-import CloseIcon from '@mui/icons-material/Close';
+import InputAdornment from '@mui/material/InputAdornment';
 import Button from '@mui/material/Button';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import { useNavigate } from 'react-router-dom';
 function LoginWindow() {
 
@@ -36,38 +38,51 @@ function LoginWindow() {
     return (
         <div className="login-window">
             <ul className="login-window-list">
-                <TextField id="email" label="Enter email" variant="outlined" />
                 <TextField
-                        required
-                        id="outlined-adornment-password1"
-                        label="Password"
-                        type={showPassword ? "text" : "password"}
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        error={errorPassword}
-                        helperText={errorPassword ? "Passwords don't match." : ""}
-                        InputProps={{
+                    className="input-field"
+                    id="email"
+                    label="Enter email"
+                    variant="outlined" 
+                    InputProps={{
                         endAdornment: (
                             <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                            >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
+                            <PersonOutlineIcon >
+                            </PersonOutlineIcon>
                             </InputAdornment>
                         ),
-                        }}
-                    />
-                <div>
+                    }}
+                />
+                <TextField
+                    className="input-field"
+                    required
+                    id="outlined-adornment-password1"
+                    label="Password"
+                    type={showPassword ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    error={errorPassword}
+                    helperText={errorPassword ? "Passwords don't match." : ""}
+                    InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                        >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                        </InputAdornment>
+                    ),
+                    }}
+                />
+                <div className="button-div">
                     <Button onClick={handleLoginClick} endIcon={<LoginIcon />}>
                         Login
                     </Button>
                 </div>
-                <div>
-                    {/*Add css styling for text size and button size*/}
+                <div className="button-div">
                     <p>
                         Don't have an account?
                             <Button onClick={handleCreateAccount}>
