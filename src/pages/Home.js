0e6a947bf2c6993866ba09191028844daffff252
Gustav <gustavpra@gmail.com>
2023-09-29 11:18:56 +0200

    Added Sidepanel button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,10 @@
-import CalenderDay from "../components/CalenderDay";
 import Background from '../components/Background';
 import Footer from '../components/Footer';
-import { SidepanelDataHome } from '../components/SidepanelData';
-import SidepanelOpen from "../components/SidepanelOpen";
 
 export default function Home()
 {
     return(
         <>
-        <SidepanelOpen SideData={SidepanelDataHome}></SidepanelOpen>
         <Background/>
         <Footer/>
         </>
