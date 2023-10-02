deda995a0e39c7ebe202c5994c390cf240e48742
Jacob <daja21sz@student.ju.se>
2023-10-02 21:37:32 +0200

    Implemented backend and database

--- backend/routes.js
+++ backend/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { request } = require('http');
 
-
 router.post('/signup',(req,res) =>
 {
     const newUser = new SignUpTemplateCpy({
