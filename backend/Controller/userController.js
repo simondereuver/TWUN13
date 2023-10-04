60a1c7ec0f40c4657174d46287c9a1826e979503
Jacob <daja21sz@student.ju.se>
2023-10-04 14:24:20 +0200

    Fixed according to PR review

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -1,62 +1,100 @@
 //Models
-const User = require('../Models');
+const User = require('../Models/Models');
 const mongoose = require('mongoose')
 
-//Get Single user
-const getUser = async (req,res) =>
-{
-    try{
-        const {id} = req.params
-            if(!mongoose.Types.ObjectId.isValid(id))
-            {
-                return res.status(404).json({error: 'User was not found'})
-            }
 
-        const user = await User.findById(id)
+//API: Endpoint: /api/users/id 
+//WHAT: Returns a user from database based on MONGO ID or email
+//USE: GET localhost.../api/users/123 (for ID)
+    //GET localhost.../api/users/bababoi@gmail.com
 
-        if(!user){
-            return res.status(404).json({ error: 'User not found' });
-        }
-        return res.status(200).json(user)
-    }
-    catch(error){
-        return res.status(500).json({message:'Server error'})
-    }
-}
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-//Create User
-    const createUser = async(req,res) => {
-        const {email,password} = req.body
-        try{
-            const user = await User.create({email,password})
-            res.status(200).json(user)
-        }
-        catch(error){
-            res.status(400).json({error: error.message})
-        }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      // If  not a valid id assume its an email
+      const user = await User.findOne({ email: id });
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      return res.status(200).json(user);
     }
 
-//Update User
-const updateUser = async (req,res) =>{
-    const {id} = req.params
-    if(!mongoose.Types.ObjectId.isValid(id)){ //Controlls that the id is a valid mongoose id
-        return res.status(404).json({error: 'User was not found'})
+    // If a valid ObjectId, find the user by ID
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
-    try{
-        const user = await User.findOneAndUpdate({_id:id}, {
-            ...req.body
-        })
-        if(!user){
-            return res.status(404).json({error: 'User was not found'})
-        }
-        res.status(200).json({message:'User was updated'})
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error' });
+  }
+};
+
+
+//API: Endpoint: /api/users/
+//WHAT: Creates a user based on a POST Request
+//USE: POST localhost.../api/users/
+        //BODY {"email": "bababoi@gmail.com", "password": "secure"}
+
+const createUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({ email, password });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//API: Endpoint: /api/users/id
+//WHAT: Updates a user on a PATCH reqqust 
+//USE: PATCH localhost.../api/users/id 
+    // PATCH localhost../api/users/bababoi@gmail.com
+        //BODY {"email": "bababoi@gmail.com", "password": "secure"}
+
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+
+  // Check if the provided parameter is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    // If it's not a valid ObjectId, assume it's an email
+    try {
+      const user = await User.findOneAndUpdate({ email: id }, { ...req.body });
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      return res.status(200).json({ message: 'User was updated' });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
     }
-    catch(error){
-        res.status(400).json({error: error.message})
+  }
+
+  // If a valid ObjectId, update the user by ID
+  try {
+    const user = await User.findByIdAndUpdate(id, { ...req.body });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
-}
+
+    return res.status(200).json({ message: 'User was updated' });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
-    createUser,
-    getUser,
-    updateUser
-}
+  createUser,
+  getUser,
+  updateUser
+};
