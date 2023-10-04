eb059cab99e376deaa4c8bc5609e96379841056a
Simon de Reuver <dereuversimon@gmail.com>
2023-10-04 18:23:40 +0200

    added cors to server file and some consolelog for error searching

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-const express = require('express')
+const express = require('express')  
 require('dotenv').config()
 const userRoutes = require('../backend/Routes/Users')
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 //express App
 const app = express()
@@ -13,11 +14,18 @@ app.get((req,res,next)=>{
     next()
 })
 
+// Enable CORS for all routes
+app.use(cors());
+
 //Routes
 app.use('/api/users',userRoutes)
 
+//console logs added for problem searching
+console.log('Before mongoose.connect');
 //Connection Database
 mongoose.connect(process.env.DATABASE_ACESS)
     .then(() => {
-        app.listen(3001,() => console.log('listening on port 3001'))})
+        console.log('Before app.listen');
+        app.listen(3001,() => console.log('listening on port 3001'))
+        console.log('After app.listen');})
     .catch((error) =>{console.log(error)})
