76eaa014097939a87e3b42bb8782bd2032d16e30
Simon de Reuver <dereuversimon@gmail.com>
2024-02-07 14:56:18 +0100

    The best changes, better then Jacob

--- Gateway/gateway.js
+++ Gateway/gateway.js
@@ -6,7 +6,15 @@ const axios = require('axios')
 
 //Routes definitions
 
-//Login Routes
+// Define the login route and forward requests to the corresponding microservice
+app.get('/api/login', async (req, res) => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/login'); // Request to Service 1: API login
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Error communicating with API login' });
+    }
+});
 
 //Calender Routes
 
