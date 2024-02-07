a02fa12fea227f9103125798c78816dbcc3f09b3
Jacob <daja21sz@student.ju.se>
2024-02-07 13:04:28 +0100

    Changed Gateway

--- Gateway/gateway.js
+++ Gateway/gateway.js
@@ -3,3 +3,20 @@ const app = express()
 const PORT = 5000;
 
 const axios = require('axios')
+
+//Routes definitions
+
+//Login Routes
+
+//Calender Routes
+
+//User Routes
+
+
+
+//Gateway Start
+
+app.listen(PORT,() => 
+{
+    console.log(`API Gateway is running on ${PORT}`)
+})
