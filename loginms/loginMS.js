b2c6a96e22b42bac11dbcd3956c4e94749493f88
Jacob <daja21sz@student.ju.se>
2024-02-29 14:25:47 +0100

    Working on RabbitMQ

---
+++ loginms/loginMS.js
@@ -0,0 +1,102 @@
+const User = require('../Models/Models');
+const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken");
+const express = require('express')  
+
+//express App
+const app = express()
+const PORT = 5003
+const amqp = require('amqplib')
+const QUEUE_SEND = "Login_User_SendQueue"
+const QUEUE_RECIEVE = "User_Login_RecieveQueue"
+const RabbitURL = "10.0.222.226"
+
+
+async function connectToRabbitMQ(queueInput) {
+    try{
+        const connection = await amqp.connect({host:RabbitURL, port: 5672});
+    }catch(error)
+    {
+        console.error(error)
+    }
+    const channel = await connection.createChannel();
+    const queue = queueInput
+    await channel.assertQueue(queue)
+    return channel
+}
+//Recieve data from recieve queue
+async function ConsumeMessage() {
+    try {
+        const queue = QUEUE_RECIEVE;
+        const channel = await connectToRabbitMQ(queue);
+        
+        console.log("Login-Microservice waiting to recieve messages in queue");
+        channel.consume(queue, async (message) => {
+            try {
+                const content = message.content;
+                return content
+            } catch (error) {
+                console.error(error);
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const loginUser = async (req, res) => {
+    try {
+       
+        const { email, password } = req.body;
+        const messageQueue = await connectToRabbitMQ(QUEUE_SEND)
+
+        //Sending a 'Get-User' to User Microservice and included email to check
+        await messageQueue.sendToQueue(QUEUE_SEND,Buffer.from(JSON.stringify(req.body)))
+        const response = await Json.stringify(ConsumeMessage())
+
+        if(response.status == 200)
+        {
+            const token = jwt.sign({email},process.env.KEY,{expiresIn:'1h'})
+            return res.status(200).json({ message: "Successfully logged in!",token:token});
+        }
+        else if(response.body.password != req.body.password)
+        {
+            return res.status(404).json({ error: 'Password' });
+        }
+        else if(response.status == 404)
+        {
+            console.log("Didnt find by email");
+            return res.status(404).json({ error: 'Email' });
+        }
+        
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error' });
+    }
+};
+
+
+
+
+
+//MiddleWare
+app.use(express.json())
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+app.post("/",loginUser)
+    
+try{
+    app.listen(PORT, () => {
+        console.log(`Server listening at ${PORT}`);
+        
+      });
+} catch(error){
+    console.error(error)
+}
+
+module.exports = {
+    connectToRabbitMQ,
+    ConsumeMessage
+}
