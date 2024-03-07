cff36c378952bcb67837fa04f30613bfa0cb1666
Gustav <gustavpra@gmail.com>
2024-03-07 13:39:09 +0100

    asd

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -24,7 +24,7 @@ function createDynamicProxy(targetIP) {
   });
 }
 
-app.use('/event', createDynamicProxy('10.0.153.35:5002'));
+app.use('/event', createDynamicProxy('10.0.153.35'));
 app.use('/users', createDynamicProxy('10.0.128.16:5001'));
 app.use('/login', createDynamicProxy('10.0.75.8:5055'));
 
